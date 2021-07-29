3fd8c169549745d997cfa2f0a8ac444cdfdd0ca5
ken <ken@somersetrecon.com>
2021-07-29 13:27:16 -0700

    Working implementation for data frames

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -1,22 +1,32 @@
 # High Level Analyzer
 # For more information and documentation, please go to https://support.saleae.com/extensions/high-level-analyzer-extensions
-
-from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
-
+from saleae.data import GraphTimeDelta
+from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, NumberSetting, ChoicesSetting
 
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class Hla(HighLevelAnalyzer):
-    # List of settings that a user can set for this High Level Analyzer.
-    my_string_setting = StringSetting()
-    my_number_setting = NumberSetting(min_value=0, max_value=100)
-    my_choices_setting = ChoicesSetting(choices=('A', 'B'))
+
+    # List of settings that a user can set for this High Level Analyzer
+    time_delimiter = NumberSetting(label='Time Delimiter [mS]', min_value=1E-6, max_value=1E6)
+    out_format = ChoicesSetting(choices=('HEX', 'DEC', 'ASCII'))
 
     # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
     result_types = {
-        'mytype': {
-            'format': 'Output type: {{type}}, Input type: {{data.input_type}}'
-        }
+        'data': {
+            'format': '{{data.data}}'
+        },
+        'error': {
+            'format': 'Error!'
+        },
     }
+
+    # Previous frame and time trackers
+    prev_frame = None
+    start_chunk_time = None
+
+    # Buffer of current chunk
+    chunk = ''
+
 
     def __init__(self):
         '''
@@ -24,18 +34,48 @@ class Hla(HighLevelAnalyzer):
 
         Settings can be accessed using the same name used above.
         '''
+        print("Time Delimiter [mS]: "+str(self.time_delimiter))
 
-        print("Settings:", self.my_string_setting,
-              self.my_number_setting, self.my_choices_setting)
+
+    def format_data(self, data):
+        '''
+        Format data given provided format setting
+        '''
+        if self.out_format == 'HEX':
+            return "{0:02X}".format(data)
+        return "{0}".format(chr(data) if self.out_format == 'ASCII' else data)
+
 
     def decode(self, frame: AnalyzerFrame):
-        '''
-        Process a frame from the input analyzer, and optionally return a single `AnalyzerFrame` or a list of `AnalyzerFrame`s.
 
-        The type and data values in `frame` will depend on the input analyzer.
-        '''
+        # support data frames from now
+        if frame.type != "data" or "data" not in frame.data.keys():
+            return None
 
-        # Return the data frame itself
-        return AnalyzerFrame('mytype', frame.start_time, frame.end_time, {
-            'input_type': frame.type
-        })
+        # special case if this is the first frame
+        if self.prev_frame is None:
+            self.prev_frame = frame
+            self.chunk = self.format_data(frame.data['data'][0])
+            self.start_chunk_time = frame.start_time
+            return None
+
+        # the AnalyzerFrame to return
+        ret_frame = None
+
+        # if time delimiter is passed create ret_frame, print chunk, and clear vars
+        if frame.start_time > self.prev_frame.end_time + GraphTimeDelta(millisecond=self.time_delimiter):
+            ret_frame = AnalyzerFrame('data', self.start_chunk_time, self.prev_frame.end_time, {
+                'data': self.chunk
+            })
+            print(self.chunk)
+            self.chunk = ''
+            self.start_chunk_time = frame.start_time
+
+        # add chunk data to buffer
+        self.chunk += self.format_data(frame.data['data'][0])
+
+        # set the previous frame end time for every frame
+        self.prev_frame = frame
+
+        # return a chunk if we hit a time delimeter otherwise None
+        return ret_frame
--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-
-  # Time Delimiter Chunking
+# Time Delimiter Chunking
   
-## Getting started
+## Description
 
-1. Build your extension by updating the Python files for your needs
-2. Create a public Github repo and push your code 
-3. Update this README
-4. Open the Logic app and publish your extension
-5. Create a Github release
-6. Debug your hardware like you've never done before :)
+Delineates chunks of data captured by a time delimiter. Aka when >3ms passes print a new line to terminal. Pretty simple.
 
-  
+## Usage
+
+1. Install this Extension
+1. Add a new Analyzer by clicking the "+" arrow in the top right of the Analyzers tab
+1. Analyze capture given the analyzer of your choice (Async Serial, SPI, etc.)
+1. Add a new High Level Analyzer by clicking the "+" arrow in the top right of the Analyzers tab
+1. Select "Time Delimiter Chunking"
+1. In the edit/settings popup set "Input Analyzer" to your previous Analyzer
+1. In the edit/settings popup set "Time Delimiter [mS]" a value between 1E-6 and 1E6
+1. Click "Save"
+1. Under the Analyzers tab select the "Data Table" icon under the "data" section
+1. Note the time delineated data displayed
+
+## Helpful Hints
+
+Disable the Low Level Analyzer Data Table display by:
+1. Selecting the three dot menu to the right of the "Type to search" box
+1. Under the "Analyzers" section select "clear"
+1. Click the "Time Delimeter Chunking" checkbox
+This will allow you to easily see the Time Delimeter Chunking Analyzer data without seeing the Low Level Analyzer data.
